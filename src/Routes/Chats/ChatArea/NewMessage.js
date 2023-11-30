068a32149ddaefb05191d8e3876b597045ed9c96
D V <divyadityasnaruka@gmail.com>
2023-11-30 07:30:30 +0530

    send on enter

--- src/Routes/Chats/ChatArea/NewMessage.js
+++ src/Routes/Chats/ChatArea/NewMessage.js
@@ -6,26 +6,38 @@ import { useRef } from 'react';
 
 const NewMessage = ({ to, fetchChats }) => {
 	const inputRef = useRef(null);
+
+	const handleKeyDown = (event) => {
+		if (event.key === 'Enter' && !event.shiftKey) {
+			event.preventDefault();
+			sendMessage(event);
+		}
+	};
+
 	const sendMessage = async (event) => {
 		event.preventDefault();
 		const message = inputRef.current.textContent.trim();
 		const serverURI = window.serverData;
-		fetch(`http://${serverURI}:49152/send-message`, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify({
-				fromUsername: JSON.parse(window.userData.userProfileData).username,
-				toUsername: to,
-				message,
-			}),
-		}).catch((error) => {
-			console.error('Error:', error);
-		});
-		inputRef.current.textContent = '';
-		fetchChats();
-		inputRef.current.focus();
+
+		if (message) {
+			fetch(`http://${serverURI}:49152/send-message`, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({
+					fromUsername: JSON.parse(window.userData.userProfileData).username,
+					toUsername: to,
+					message,
+				}),
+			}).catch((error) => {
+				console.error('Error:', error);
+			});
+
+			inputRef.current.textContent = '';
+			fetchChats();
+			inputRef.current.focus();
+		}
 	};
 
 	return (
@@ -43,10 +55,10 @@ const NewMessage = ({ to, fetchChats }) => {
 					spellCheck="true"
 					title="Type a message"
 					ref={inputRef}
+					onKeyDown={handleKeyDown}
 				>
 					Type a message
 				</div>
-				{/* <div className={styles['writtable-placeholder']}>Type a message</div> */}
 			</div>
 			<div className={styles['send-btn-wrapper']}>
 				<button
